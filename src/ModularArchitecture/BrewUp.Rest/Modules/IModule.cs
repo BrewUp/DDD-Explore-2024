9790a469e4413c08e16348d05484095c6fb4713d
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-02-12 19:54:12 +0100

    Implement Sales.Domain

---
+++ src/ModularArchitecture/BrewUp.Rest/Modules/IModule.cs
@@ -0,0 +1,10 @@
+﻿namespace BrewUp.Rest.Modules;
+
+public interface IModule
+{
+	bool IsEnabled { get; }
+	int Order { get; }
+
+	IServiceCollection RegisterModule(WebApplicationBuilder builder);
+	IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints);
+}
