d7eab5bef1966166ee4508695605519878de4214
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-02-17 13:28:46 +0100

    Adding SalesInfrastructure

---
+++ src/Sales/BrewUp.Sales.Infrastructures/RabbitMq/Commands/CreateSalesOrderConsumer.cs
@@ -0,0 +1,17 @@
+﻿using BrewUp.Sales.Domain.CommandHandlers;
+using BrewUp.Sales.SharedKernel.Commands;
+using Microsoft.Extensions.Logging;
+using Muflone.Messages.Commands;
+using Muflone.Persistence;
+using Muflone.Transport.RabbitMQ.Abstracts;
+using Muflone.Transport.RabbitMQ.Consumers;
+
+namespace BrewUp.Sales.Infrastructures.RabbitMq.Commands;
+
+public sealed class CreateSalesOrderConsumer(IRepository repository,
+		IMufloneConnectionFactory connectionFactory,
+		ILoggerFactory loggerFactory)
+	: CommandConsumerBase<CreateSalesOrder>(repository, connectionFactory, loggerFactory)
+{
+	protected override ICommandHandlerAsync<CreateSalesOrder> HandlerAsync { get; } = new CreateSalesOrderCommandHandler(repository, loggerFactory);
+}
